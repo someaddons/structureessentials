ac6cffc47e0e3b38255621c4d36c49eed43434d8
someaddons <someaddons@github.com>
2023-04-29 16:22:05 +0200

    2.6: Add crash prevention for occasional structure mod updates breaking worlds.

---
+++ src/main/java/com/structureessentials/mixin/MappedRegistryMixin.java
@@ -0,0 +1,37 @@
+package com.structureessentials.mixin;
+
+import com.mojang.serialization.Lifecycle;
+import com.structureessentials.StructureEssentials;
+import net.minecraft.core.MappedRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(value = MappedRegistry.class, priority = 10000)
+public abstract class MappedRegistryMixin extends Registry
+{
+    protected MappedRegistryMixin(final ResourceKey p_122920_, final Lifecycle p_122921_)
+    {
+        super(p_122920_, p_122921_);
+    }
+
+    @Redirect(method = "freeze", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z"), require = 0)
+    private boolean onfreeze(final List instance)
+    {
+        if (!StructureEssentials.config.getCommonConfig().warnMissingRegistryEntry)
+        {
+            return instance.isEmpty();
+        }
+
+        if (!instance.isEmpty())
+        {
+            StructureEssentials.LOGGER.error("Unbound values in registry " + key() + ": " + instance);
+        }
+
+        return true;
+    }
+}
