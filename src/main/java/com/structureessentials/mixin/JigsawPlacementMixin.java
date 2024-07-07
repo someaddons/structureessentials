6aa4b85413388a99cf3a979d5943cb2fddfa7d2e
someaddons <someaddons@github.com>
2024-07-07 23:57:34 +0200

    1.21

--- src/main/java/com/structureessentials/mixin/JigsawPlacementMixin.java
+++ src/main/java/com/structureessentials/mixin/JigsawPlacementMixin.java
@@ -5,8 +5,11 @@ import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.pools.DimensionPadding;
 import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.pools.alias.PoolAliasLookup;
+import net.minecraft.world.level.levelgen.structure.templatesystem.LiquidSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,9 +19,21 @@ import java.util.Optional;
 
 @Mixin(JigsawPlacement.class)
 public class JigsawPlacementMixin {
-    @Inject(method = "addPieces(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;Lnet/minecraft/core/Holder;Ljava/util/Optional;ILnet/minecraft/core/BlockPos;ZLjava/util/Optional;I)Ljava/util/Optional;"
+    @Inject(method = "addPieces(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;Lnet/minecraft/core/Holder;Ljava/util/Optional;ILnet/minecraft/core/BlockPos;ZLjava/util/Optional;ILnet/minecraft/world/level/levelgen/structure/pools/alias/PoolAliasLookup;Lnet/minecraft/world/level/levelgen/structure/pools/DimensionPadding;Lnet/minecraft/world/level/levelgen/structure/templatesystem/LiquidSettings;)Ljava/util/Optional;"
             , at = @At("HEAD"), cancellable = true)
-    private static void essentials$addPieces(Structure.GenerationContext generationContext, Holder<StructureTemplatePool> holder, Optional<ResourceLocation> optional, int i, BlockPos blockPos, boolean bl, Optional<Heightmap.Types> optional2, int j, CallbackInfoReturnable<Optional<Structure.GenerationStub>> cir) {
+    private static void essentials$addPieces(
+      final Structure.GenerationContext generationContext,
+      final Holder<StructureTemplatePool> holder,
+      final Optional<ResourceLocation> optional,
+      final int i,
+      final BlockPos blockPos,
+      final boolean bl,
+      final Optional<Heightmap.Types> optional2,
+      final int j,
+      final PoolAliasLookup poolAliasLookup,
+      final DimensionPadding dimensionPadding,
+      final LiquidSettings liquidSettings,
+      final CallbackInfoReturnable<Optional<Structure.GenerationStub>> cir) {
         if (!holder.isBound()) {
             cir.setReturnValue(Optional.empty());
         }
